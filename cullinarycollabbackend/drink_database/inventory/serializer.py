9688945efb3f65ba234c2bd43f479fb72264ad40
Jrobbinsss18 <jrobbinsss18@gmail.com>
2023-11-18 16:51:29 -0800

    Removing unneeded files, renaming frontend dir

--- cullinarycollabbackend/drink_database/inventory/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class ReactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Drink
-        fields = ['name', 'ingredients', 'instructions']
-
-
