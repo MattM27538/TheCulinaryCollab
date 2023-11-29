c86bf8936fc7b2c88522bb9c3b3eac6d52023266
Bryan Payne <bkpayne@csuchico.edu>
2023-11-29 10:16:50 -0800

    fixed minor error in landing page`

--- CulinaryCollabFrontend/src/App.js
+++ CulinaryCollabFrontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './components/LandingPage';
-import AboutPage from './components/AboutPage';
 import PublicRecipesPage from './components/PublicRecipesPage';
 import WorkshopPage from './components/WorkshopPage';
 import BarHelpPage from './components/BarHelpPage';
@@ -17,7 +16,6 @@ function App() {
                 <Routes>
 	            <Route path="/" element={<LandingPage />} />
                     <Route path="/login" element={<LoginPage />} />
-                    <Route path="/about" element={<AboutPage />} />
                     <Route path="/public-recipes" element={<PublicRecipesPage />} />
                     <Route path="/workshop" element={<WorkshopPage />} />
 	            <Route path="/bar-help" element={<BarHelpPage />} />
