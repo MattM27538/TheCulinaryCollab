295b650b964ed292adce96b22d1cdabf858c537c
Jrobbinsss18 <jrobbinsss18@gmail.com>
2023-11-30 23:36:57 -0800

    Adding ability to rate other users recipes

--- CulinaryCollabFrontend/src/components/ViewRecipeModal.js
+++ CulinaryCollabFrontend/src/components/ViewRecipeModal.js
@@ -1,14 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import RatingStars from './RatingStars';
 import './ViewRecipeModal.css';
-import { auth } from '../firebase';
+import { auth, firestore } from '../firebase';
+import { doc, setDoc, getDocs, collection } from 'firebase/firestore';
+
 const ViewRecipeModal = ({ isOpen, onClose, recipe, onSave, showSaveOption }) => {
-	if (!recipe) return null;
+	const [userRating, setUserRating] = useState(null);
+	const [averageRating, setAverageRating] = useState(null);
+	const user = auth.currentUser;
 	const isLoggedIn = auth.currentUser != null;
+	const fetchRatings = async () => {
+		if (!recipe) return;
+
+		const ratingsRef = collection(firestore, `recipes/${recipe.id}/ratings`);
+		const ratingsSnap = await getDocs(ratingsRef);
+		let total = 0;
+		let count = 0;
+		let currentUserRating = null;
+
+		ratingsSnap.forEach((doc) => {
+			total += doc.data().rating;
+			count++;
+			if (doc.id === user.uid) {
+				currentUserRating = doc.data().rating;
+			}
+		});
+
+		setAverageRating(count === 0 ? 0 : total / count);
+		setUserRating(currentUserRating);
+	};
+
+	useEffect(() => {
+		if (recipe && isOpen) {
+			fetchRatings();
+		}
+	}, [recipe, isOpen]);
+
+	const handleRating = async (newRating) => {
+		if (user) {
+			const ratingRef = doc(firestore, `recipes/${recipe.id}/ratings`, user.uid);
+			await setDoc(ratingRef, { rating: newRating });
+			setUserRating(newRating);
+			fetchRatings();
+		}
+	};
+
+	if (!recipe) return null;
 
 	return (
 		<div className={`modal ${isOpen ? 'open' : ''}`}>
 		<div className="modal-content">
 		<h2>{recipe.name}</h2>
+
+		<div>
+		<p>Your Rating:</p>
+		<RatingStars value={userRating || 0} onChange={handleRating} />
+		</div>
+
+		{averageRating !== null && (
+			<div>
+			<p>Average Rating:</p>
+			<RatingStars value={averageRating} readOnly />
+			</div>
+		)}
 		<p><strong>Timing:</strong> {recipe.timing}</p>
 		<p><strong>Taste:</strong> {recipe.taste}</p>
 		<p><strong>Ingredients:</strong></p>
@@ -27,7 +81,6 @@ const ViewRecipeModal = ({ isOpen, onClose, recipe, onSave, showSaveOption }) =>
 		</div>
 		</div>
 	);
-
 };
 
 export default ViewRecipeModal;
